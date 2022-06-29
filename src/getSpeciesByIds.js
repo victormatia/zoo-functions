7e424f27653332ddd09eba677336201d7c8bde7c
Victor Matias <fvictor1705@gmail.com>
2022-06-29 15:39:46 -0300

    Resolve requisito 1.

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui VQV
+function getSpeciesByIds(...ids) {
+  if (ids === undefined) return [];
+  return ids.map((id) => data.species.find((specie) => specie.id === id));
 }
 
 module.exports = getSpeciesByIds;

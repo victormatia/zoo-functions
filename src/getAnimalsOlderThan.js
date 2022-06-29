4db2366cb006e06708f80c04d5893398797b6793
Victor Matias <fvictor1705@gmail.com>
2022-06-29 16:52:24 -0300

    Resolve requisito 2.

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const selectedAnimal = data.species.find((specie) => specie.name === animal); // retorna obj de animal selecionado.
+  const allAnimals = selectedAnimal.residents; // retorna um array com todos os animais da especie desejada.
+  return allAnimals.every((e) => e.age >= age); // verifica se as idades são maiores ou iguais a age
 }
 
 module.exports = getAnimalsOlderThan;

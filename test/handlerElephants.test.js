e60b08f5c40f2e3c2fb2d9097a4b088776691efe
Victor Matias <fvictor1705@gmail.com>
2022-06-30 15:51:31 -0300

    Resolve requisito 6.

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,27 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  test('Testa se quando a função não recebe parâmetro o retorno é undefined.', () => {
+    expect(handlerElephants()).toBe(undefined);
+  });
+  test('Testa se quando a função recebe um parâmetro diferente de uma string o retorno é o esperado.', () => {
+    const expected = 'Parâmetro inválido, é necessário uma string';
+    expect(handlerElephants(['names'])).toBe(expected);
+  });
+  test('Testa se quando a função recebe um parâmetro diferente dos esperados o retorno é null.', () => {
+    expect(handlerElephants('heigth')).toBeNull();
+  });
+  test('Testa se quando a função recebe o parâmetro count o retorno é 4.', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+  test('Testa se quando a função recebe o parâmetro names o retorno é um array com todos os nomes dos elefantes.', () => {
+    const names = ['Ilana', 'Ilana', 'Bea', 'Jefferson'];
+    expect(handlerElephants('names')).toEqual(names);
+  });
+  test('Testa se quando a função recebe o parâmetro avarageAge o retorno é a média das idades dos elefantes', () => {
+    expect(handlerElephants('averageAge')).toBeCloseTo(10.5);
+  });
+  test('Testa se quando a função recebe o parâmetro location o retorno a localização dos elefantes dentro do zoológico', () => {
+    expect(handlerElephants('location')).toBe('NW');
+  });
+});

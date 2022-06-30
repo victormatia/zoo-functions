f6231dd8810476fb8fe17b27e5a76637ad282e85
Victor Matias <fvictor1705@gmail.com>
2022-06-30 17:27:50 -0300

    Resolve requisito 8. Obs.: O commit anterior resolveu os requisitos 6, 7, 15.

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,21 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const onlyChilds = entrants.filter((entrant) => entrant.age < 18).length;
+  const onlyAdults = entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50).length;
+  const onlySeniors = entrants.filter((entrant) => entrant.age >= 50).length;
+  return { adult: onlyAdults, child: onlyChilds, senior: onlySeniors };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.entries(entrants).length === 0) return 0;
+  const { adult, child, senior } = countEntrants(entrants);
+  const amount = {
+    adults: 49.99 * adult,
+    childs: 20.99 * child,
+    seniors: 24.99 * senior,
+  };
+  return amount.adults + amount.childs + amount.seniors;
 }
 
 module.exports = { calculateEntry, countEntrants };

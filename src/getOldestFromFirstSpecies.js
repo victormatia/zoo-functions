759b02be9cb560a233e3fc821660bb9812318790
Victor Matias <fvictor1705@gmail.com>
2022-07-01 15:21:58 -0300

    Resolve requisito 10.

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
+const { species, employees } = data;
+
+const findEmployee = (id) => employees.find((employee) => employee.id === id);
+
+const getFirstAnimal = (employee) => species.find((spec) => spec.id === employee.responsibleFor[0]);
+
+const findOldestAnimal = (animals) => {
+  const olderAge = animals.residents.reduce((acc, curr) => Math.max(acc, curr.age), 0);
+  return animals.residents.find((animal) => animal.age === olderAge);
+};
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const selectedAnimals = getFirstAnimal(findEmployee(id));
+  return Object.values(findOldestAnimal(selectedAnimals));
 }
 
 module.exports = getOldestFromFirstSpecies;
